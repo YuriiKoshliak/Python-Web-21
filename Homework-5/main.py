67077e18c0295a90222d9d4fa14cd48461b176ec
Yurii Koshliak <satorxp10@gmail.com>
2024-04-11 21:53:56 +0200

    Додано обробку помилок

--- Homework-5/main.py
+++ Homework-5/main.py
@@ -10,8 +10,14 @@ class CurrencyRate:
         self.api_url = "https://api.privatbank.ua/p24api/exchange_rates?json&date="
 
     async def fetch(self, session, url):
-        async with session.get(url) as response:
-            return await response.text()
+        try:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                return await response.text()
+        except aiohttp.ClientError as e:
+            print(f"Помилка при запиті до {url}: {e}")
+        except json.JSONDecodeError as e:
+            print(f"Помилка при перетворенні відповіді в JSON: {e}")
 
     async def get_currency_rate(self):
         currency_data = []
@@ -19,17 +25,17 @@ class CurrencyRate:
             for day in range(self.days):
                 date = datetime.now() - timedelta(days=day)
                 formatted_date = date.strftime("%d.%m.%Y")
-                url = self.api_url + date.strftime("%d.%m.%Y")
+                url = f"{self.api_url}{formatted_date}"
                 response = await self.fetch(session, url)
-                data = json.loads(response)
-                eur = usd = None
-                for exchange_rate in data['exchangeRate']:
-                    if exchange_rate['currency'] == 'EUR':
-                        eur = {'sale': exchange_rate['saleRate'], 'purchase': exchange_rate['purchaseRate']}
-                    elif exchange_rate['currency'] == 'USD':
-                        usd = {'sale': exchange_rate['saleRate'], 'purchase': exchange_rate['purchaseRate']}
-                if eur and usd:
-                    currency_data.append({formatted_date: {'EUR': eur, 'USD': usd}})
+                if response:
+                    try:
+                        data = json.loads(response)
+                        rates = {rate['currency']: {'sale': rate['saleRate'], 'purchase': rate['purchaseRate']}
+                                 for rate in data['exchangeRate'] if rate['currency'] in ['EUR', 'USD']}
+                        if rates:
+                            currency_data.append({formatted_date: rates})
+                    except KeyError as e:
+                        print(f"Помилка при доступі до даних курсу валют: {e}")
         return currency_data
 
 if __name__ == "__main__":
